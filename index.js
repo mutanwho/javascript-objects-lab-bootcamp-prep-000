0f0f25596f0d04e90ee199f1d7704dcd673f7f74
Michelle M <mutanwho@gmail.com>
2018-10-18 16:23:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function destructivelyDeleteFromObjectByKey(object, key){
 
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object['key']
+  delete object[key]
   return object;
 };
 
